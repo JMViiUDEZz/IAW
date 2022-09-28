e8251a4dbb21f81be7cf7d6cdcce0abfa9e3ae35
José María Viúdez <jmviudezp2003@gmail.com>
2022-09-28 14:25:13 +0200

    consultas

--- Nest-Backend/02-car-dealership/src/cars/cars.controller.ts
+++ Nest-Backend/02-car-dealership/src/cars/cars.controller.ts
@@ -1,4 +1,27 @@
-import { Controller } from '@nestjs/common';
+/* eslint-disable prettier/prettier */
+import { Controller, Get } from '@nestjs/common';
 
 @Controller('cars')
-export class CarsController {}
+export class CarsController {
+  
+  @Get('list')
+  getAllCars() {
+    return ['Listado de Cars'];
+  }
+
+  @Get('new')
+  newCar() {
+    return ['Creado de Cars'];
+  }
+
+  @Get('update')
+  updateCar() {
+    return ['Actualizado de Cars'];
+  }
+
+  @Get('delete')
+  deleteCar() 
+  {
+    return ['Borrado de Cars'];
+  }
+}
