d8c0f26ea75f7d5771f19d162af463008ed8075d
José María Viúdez <jmviudezp2003@gmail.com>
2022-11-03 14:31:33 +0100

    consultas create y findAll de clientes

--- Nest-Backend/pLibreria/api-libreria/src/modules/clientes/dto/create-cliente.dto.ts
+++ Nest-Backend/pLibreria/api-libreria/src/modules/clientes/dto/create-cliente.dto.ts
@@ -1,1 +1,33 @@
-export class CreateClienteDto {}
+import { IsIn, IsOptional, IsString, MinLength } from "class-validator";
+
+export class CreateClienteDto {
+    @IsString()
+    @MinLength(1)
+    name: string;
+  
+    @IsString()
+    @MinLength(1)
+    email: string;
+  
+    @IsString()
+    @MinLength(1)
+    @IsOptional()
+    website?: string;
+  
+    @IsString()
+    @MinLength(1)
+    @IsOptional()
+    github?: string;
+  
+    @IsString()
+    @MinLength(1)
+    @IsOptional()
+    twitter?: string;
+
+    @IsString()
+    @MinLength(1)
+    @IsOptional()
+    @IsIn(['anaya', 'marcombo'])
+    editorial?: string;
+ }
+ 
--- Nest-Backend/pLibreria/api-libreria/src/modules/clientes/entities/cliente.entity.ts
+++ Nest-Backend/pLibreria/api-libreria/src/modules/clientes/entities/cliente.entity.ts
@@ -2,26 +2,36 @@ import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
 
 @Entity({ name: 'clientes' })
 export class Cliente {
-   @PrimaryGeneratedColumn('increment')
-   id: number;
+  @PrimaryGeneratedColumn('increment')
+  id: number;
+
+  @Column('text', {
+    unique: true,
+  })
+  name: string;
+
+  @Column('text', {
+    unique: true,
+  })
+  email: string;
+
+  @Column('text', {
+    unique:true,
+  })
+  website?: string;   
  
-   @Column('text', {
-        unique: true,
-   })
-   name: string;
+  @Column('text', {
+    unique: true,
+  })
+  github?: string;
  
-   @Column('text', {
-        unique: true,
-   })
-   email: string;
- 
-   @Column('text', {
-        unique: true,
-   })
-   github: string;
- 
-   @Column('text', {
-        unique:true,
-   })
-   twitter: string;
+  @Column('text', {
+    unique:true,
+  })
+  twitter?: string;
+
+  @Column('text', {
+    unique:true,
+  })
+  editorial?: string;
 }
