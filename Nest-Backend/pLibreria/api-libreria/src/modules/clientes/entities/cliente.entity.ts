e312b1a4d056bcdce6da769ebaebf1174a11ef8c
José María Viúdez <jmviudezp2003@gmail.com>
2022-11-09 13:31:33 +0100

    actualizacion entidades libro y cliente

--- Nest-Backend/pLibreria/api-libreria/src/modules/clientes/entities/cliente.entity.ts
+++ Nest-Backend/pLibreria/api-libreria/src/modules/clientes/entities/cliente.entity.ts
@@ -2,36 +2,33 @@ import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
 
 @Entity({ name: 'clientes' })
 export class Cliente {
-  @PrimaryGeneratedColumn('increment')
-  id: number;
+     @PrimaryGeneratedColumn('increment')
+     id: number;
 
-  @Column('text', {
-    unique: true,
-  })
-  name: string;
-
-  @Column('text', {
-    unique: true,
-  })
-  email: string;
-
-  @Column('text', {
-    unique:true,
-  })
-  website?: string;   
+     @Column('text', {
+          unique: true,
+     })
+     name: string;
  
-  @Column('text', {
-    unique: true,
-  })
-  github?: string;
- 
-  @Column('text', {
-    unique:true,
-  })
-  twitter?: string;
+     @Column('text', {
+          unique: true,
+     })
+     email: string;
 
-  @Column('text', {
-    unique:true,
-  })
-  editorial?: string;
+     @Column('text', {
+          unique:true,
+     })
+     website?: string;   
+ 
+     @Column('text', {
+          unique: true,
+     })
+     github?: string;
+ 
+     @Column('text', {
+          unique:true,
+     })
+     twitter?: string;
+
+    //  books?: string;
 }
--- Nest-Backend/pLibreria/api-libreria/src/modules/libros/entities/libro.entity.ts
+++ Nest-Backend/pLibreria/api-libreria/src/modules/libros/entities/libro.entity.ts
@@ -2,22 +2,49 @@ import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
 
 @Entity({ name: 'libros' })
 export class Libro {
-   @PrimaryGeneratedColumn('increment')
-   id: number;
+     @PrimaryGeneratedColumn('increment')
+     id: number;
  
-   @Column('text', {
-        unique: true,
-   })
-   name: string;
- 
-   @Column('text', {
-        unique: true,
-   })
-   titulo: string;
+     @Column('text', {
+          unique: true,
+     })
+     title: string;
 
-   @Column('text', {
-        unique:true,
-    })
-    editorial: string;
+     @Column('text', {
+          unique: true,
+     })
+     isbn: string;
+
+     @Column('integer', {
+          unique:true,
+     })
+     pageCount: number;
+
+     @Column('text', {
+          unique:true,
+     })
+     publishedDate: string;
+
+     @Column('text', {
+          unique:true,
+     })
+     thumbnailUrl: string;
+
+     @Column('text', {
+          unique:true,
+     })
+     shortDescription: string;
+
+     @Column('text', {
+          unique:true,
+     })
+     longDescription: string;
+
+     @Column('text', {
+          unique:true,
+     })
+     status: string;
+
+     // authors: string;
 
 }
